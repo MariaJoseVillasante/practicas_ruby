4f22a3fc4dbaba9af9113ac6f841122d0e3b217b
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-07 18:54:32 -0400

    agregando patrones

--- ciclos_y_metodos/patrones_y_ciclos_anidados/patrones.rb
+++ ciclos_y_metodos/patrones_y_ciclos_anidados/patrones.rb
@@ -1,61 +1,117 @@
-letra = ARGV[0];
+#letra = ARGV[0];
 
-if letra == "n"
-    letra.times do |i|
-        letra.times do |j|
+def letra_o(n)
+    #Parte superior
+    n.times do
         print "*"
+    end
+    print "\n"
+
+    #Parte del medio
+    (n - 2).times do
+        print "*"
+        (n - 2).times do 
+            print " "
         end
+        print "*"
+        print "\n"
+    end
+
+    # Parte inferior
+    n.times do |i|
+        print "*"
+    end
+    
+        # puts "*****"
+        # puts "*   *"
+        # puts "*   *"
+        # puts "*   *"
+        # puts "*****"
+end
+
+def letra_i(n)
+    #Parte superior
+    n.times do
+        print "*"
+    end
+    print "\n"
+
+    #Parte del medio
+    (n-2).times do
+        print " "
+        (n/2-1).times do 
+            print " "
+        end
+        print "*"
         print "\n"
     end
     
-    puts "*****"
-    puts "*   *"
-    puts "*   *"
-    puts "*   *"
-    puts "*****"
-    
-elsif letra == "0"
-    puts "*****"
-    puts "  *  "
-    puts "  *   "
-    puts "  *  "
-    puts "*****"
-elsif letra == "z"
-    puts "*****"
-    puts "   *"
-    puts " *   "
-    puts "*  "
-    puts "*****"
-elsif letra == "x"
+    # Parte inferior
+    n.times do |i|
+        print "*"
+    end
+    # puts "*****"
+    # puts "  *  "
+    # puts "  *   "
+    # puts "  *  "
+    # puts "*****"
+end
+
+def letra_z(n)
+    #Parte superior
+    n.times do
+        print "*"
+    end
+    print "\n"
+
+    #Parte del medio
+    (n - 2).times do |i|
+       # print "*"
+        (n-i-2).times do |j|
+            print " "
+        end
+        print "*"
+        print "\n"
+    end
+
+    # Parte inferior
+    n.times do |i|
+        print "*"
+    end
+    # puts "*****"
+    # puts "   *"
+    # puts " *   "
+    # puts "*  "
+    # puts "*****"
+end
+
+def letra_x(n)
     puts "*   *"
     puts " * * "
     puts "  *   "
     puts " * *"
     puts "*   *"
-elsif letra == "z"
-    puts "*   *"
-    puts " * * "
-    puts "  *   "
-    puts " * *"
-    puts "*   *"
-elsif letra == "0"
+end
+
+def numero_cero(n)
     puts "*****"
     puts "**  *"
     puts "* * *"
     puts "*  **"
     puts "*****"
-elsif letra == "0"
-    puts "*   *"
-    puts " * * "
-    puts "  *   "
-    puts " * *"
-    puts "*   *"
-elsif letra == "0"
+end
+
+def navidad(n)
     puts "   *"
     puts "  * * "
     puts " * * *"
     puts "* * * *"
     puts "   *   "
-else
-    
 end
+
+#letra_o(5)
+#letra_i(5)
+letra_z(5)
+##letra_x(5)
+#numero_cero(8)
+#navidad(5)
