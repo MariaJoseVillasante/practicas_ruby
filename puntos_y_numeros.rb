5a6ceec86d92999a97bc407e7ac9d9e15beb8639
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-09 10:11:13 -0400

    agregando simple arrays

--- puntos_y_numeros.rb
+++ puntos_y_numeros.rb
@@ -1,10 +1,24 @@
-print "Cuantos numeros quiere imprimir"
+print "Cuantos numeros quiere imprimir: "
 n = gets.chomp.to_i
-#(0..n).each {|i| print "#{i}."}
 
-(1..n).each do |i| 
-    print "#{i}."
+(1..n).each do |i|
+    if i.even?
+        print i
+    else
+        print "."
+    end
 end
 
+#(0..n).each {|i| print "#{i}."}
+#ingresa pares de números alternando numeros y puntos
+=begin
+    
+rescue => exception
+    
+end(1..n).each do |i| 
+    print "#{i}."
+end
+=end
+#para imprimir abecedario
 # abecedario=[*'A'..'Z']
 # print abecedario
---
+++ simple_array.rb
@@ -0,0 +1,13 @@
+a = [5,6,4,9,12,1,0]
+print a
+puts a[0]
+puts a[-1]
+puts a[2,4]
+puts a[-2,3]
+puts "#{a[0]} #{a[5]}"
+b = ['Tlaxcala', 'Puebla','Veracruz','Tabasco']
+print b 
+puts ""
+puts b[1]
+puts b[-3]
+
