235886bd0a385a2b773eab361c945d45d46837e4
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-09 09:47:42 -0400

    intentandi el arbol

--- ciclos_y_metodos/patrones_y_ciclos_anidados/patrones.rb
+++ ciclos_y_metodos/patrones_y_ciclos_anidados/patrones.rb
@@ -6,7 +6,6 @@ def letra_o(n)
         print "*"
     end
     print "\n"
-
     #Parte del medio
     (n - 2).times do
         print "*"
@@ -16,7 +15,6 @@ def letra_o(n)
         print "*"
         print "\n"
     end
-
     # Parte inferior
     n.times do |i|
         print "*"
@@ -35,17 +33,14 @@ def letra_i(n)
         print "*"
     end
     print "\n"
-
     #Parte del medio
     (n-2).times do
-        print " "
-        (n/2-1).times do 
+        (n/2).times do 
             print " "
         end
         print "*"
         print "\n"
     end
-    
     # Parte inferior
     n.times do |i|
         print "*"
@@ -63,17 +58,14 @@ def letra_z(n)
         print "*"
     end
     print "\n"
-
     #Parte del medio
     (n - 2).times do |i|
-       # print "*"
         (n-i-2).times do |j|
             print " "
         end
         print "*"
         print "\n"
     end
-
     # Parte inferior
     n.times do |i|
         print "*"
@@ -86,32 +78,78 @@ def letra_z(n)
 end
 
 def letra_x(n)
-    puts "*   *"
-    puts " * * "
-    puts "  *   "
-    puts " * *"
-    puts "*   *"
+    #Parte superior
+    n.times do |i|
+        (n).times do |j|
+            if j==i || n-1==j+i
+                print "*"
+            else 
+                print " "
+            end
+        end
+        print "\n"
+        
+    end
+    
+    # puts "*   *"
+    # puts " * * "
+    # puts "  *   "
+    # puts " * *"
+    # puts "*   *"
 end
 
 def numero_cero(n)
-    puts "*****"
-    puts "**  *"
-    puts "* * *"
-    puts "*  **"
-    puts "*****"
+    #Parte superior
+    (n+1).times do
+        print "*"
+    end
+    print "\n"
+    #Parte del medio
+    (n - 2).times do |i|
+       # print "*"
+        (n).times do |j|
+            if j==(i+1) || j== 0 #|| j== (i+1)#i=n # (n-1) #|| j== (i+1)
+                print "*"
+            else 
+                print " "
+            end
+        end
+        print "*"
+        print "\n"
+    end
+    # Parte inferior
+    (n+1).times do |i|
+        print "*"
+    end
+    # puts "*****"
+    # puts "**  *"
+    # puts "* * *"
+    # puts "*  **"
+    # puts "*****"
 end
 
 def navidad(n)
-    puts "   *"
-    puts "  * * "
-    puts " * * *"
-    puts "* * * *"
-    puts "   *   "
+    n.times do |i|
+        n.times do |j|
+            if j==(n/2-i) || i==(n/2) || j==(n/2+i) || j==n/2# || i==n #&& 2<i<n 
+                print "*"
+            else 
+                print " "
+            end
+        end
+        print "\n"
+    end
+    
+    # puts "   *"
+    # puts "  * * "
+    # puts " * * *"
+    # puts "* * * *"
+    # puts "   *   "
 end
 
 #letra_o(5)
-#letra_i(5)
-letra_z(5)
-##letra_x(5)
-#numero_cero(8)
-#navidad(5)
+#letra_i(3)
+#letra_z(5)
+#letra_x(5)
+#numero_cero(5)
+navidad(18)
