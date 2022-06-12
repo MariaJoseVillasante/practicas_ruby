1d14cbf87e87e2d6314431400d631fc98df34445
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-11 21:08:59 -0400

    suma v2

---
+++ ciclos_y_metodos/lista_html.rb
@@ -0,0 +1,7 @@
+numero = ARGV[0].to_i
+
+puts "<ul>"
+(numero+1).times do |i|
+    puts "\t<li>#{i}</li>"
+end
+puts "<ul>"
--- ciclos_y_metodos/suma_n.rb
+++ ciclos_y_metodos/suma_n.rb
@@ -1,7 +1,4 @@
 numero = ARGV[0].to_i
-suma = 0
-(numero+1).times do |i|
-    suma += i
-end
 
-print suma
+i = (1..numero).sum
+puts i
