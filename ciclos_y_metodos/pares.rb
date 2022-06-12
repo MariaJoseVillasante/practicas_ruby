f0b4bf0c6b457dc15467ee74ab404cba688f4525
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-11 21:17:27 -0400

    Pares v2

--- ciclos_y_metodos/pares.rb
+++ ciclos_y_metodos/pares.rb
@@ -1,9 +1,18 @@
 numero = ARGV[0].to_i
 suma = 0
-(numero+1).times do |i|
+#i = (1..numero)
+(1..numero).each do |i|
     if i.even?
         suma += i
     end
 end
+puts suma
+
+# suma = 0
+# (numero+1).times do |i|
+#     if i.even?
+#         suma += i
+#     end
+# end
 
 print suma
