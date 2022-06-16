b9941fe471458e62360a978887e0ae06b274688e
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-16 18:01:59 -0400

    Arreglando figuras_strings

--- ciclos_y_metodos/figuras_strings.rb
+++ ciclos_y_metodos/figuras_strings.rb
@@ -5,46 +5,68 @@ def menu
     puts "3. Pirámide"
     puts "4. Salir"
 end
-def eleccion_jugador(jugador)
-   # jugador = gets.chomp.downcase
+def eleccion_jugador
+   jugador = gets.chomp.to_i
     while jugador != 1 && jugador != 2 && jugador != 3 && jugador != 4
         puts "Ingrese una opción valida"
-        break
-        if jugador == "1"
-            print "Jugador eligio cuadrado"
-        elsif jugador == "2"
-            print "Jugador eligio triángulo"
-        elsif jugador == "3" 
-            print "Jugador eligio pirámide"
+        jugador = gets.chomp.to_i
+        if  jugador == 4  
+            exit
         end
-        #if else       
     end
+    if jugador == 1
+        eleccion_jugador = 'Cuadrado'
+    elsif jugador == 2
+        eleccion_jugador = 'Triángulo'
+    elsif jugador == 3
+        eleccion_jugador = 'Pirámide'
+    end
+    print "El jugador eligió #{eleccion_jugador}.  "
     return jugador
 end
-
-def cuadrado(n)
-    n.times do|i|
-        n.times do|j|
-            print "*"
-        end
+def eleccion_figura
+    n = gets.chomp.to_i
+    while n < 0 || n > 10
+        puts "Ingresa un número entre 1 y 10"
+        n = gets.chomp.to_i
     end
+    return n
+end
+def figura(jugador,n)
+    if jugador == 1
+        n.times do|i|   
+            n.times do|j|
+                print "*"
+            end
+            puts '*'
+        end
+    elsif jugador == 2
+        n.times do|i|   
+            i.times do|j|
+                print "*"
+            end
+            puts '*'
+        end
+    elsif jugador == 3
+        n.times do|i|   
+            i.times do|j|
+                print "*"
+            end
+            puts '*'
+        end
+        n.times do|i|   
+            (n-i).times do|j|
+                print "*"
+            end
+            puts '*'
+        end
+        puts '*'
+    end          
 end
 
-def triangulo(n)
-end
-
-def piramide(n)
-end
 menu 
-puts "Jugador 1 por favor elige una opcion: 1, 2, 3 o 4"
-jugador = gets.chomp.downcase
-eleccion_jugador(jugador)
+puts "Jugador, por favor elige una opcion: 1, 2, 3 o 4"
+jugador = eleccion_jugador
 puts "Ingresa un número: "
-n = gets.chomp.to_i
-if jugador == 1
-    print cuadrado(n) 
-elsif jugador == 2
-    print triangulo(n)
-elsif jugador == 3
-    print piramide(n)
-end
+n = eleccion_figura #gets.chomp.to_i
+figura(jugador,n)
