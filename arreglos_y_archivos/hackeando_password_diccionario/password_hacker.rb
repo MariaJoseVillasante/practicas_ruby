70c1caea7117720f69f963a2bd50c6982d21d4be
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-18 12:25:32 -0400

    act password

--- arreglos_y_archivos/hackeando_password_diccionario/password_hacker.rb
+++ arreglos_y_archivos/hackeando_password_diccionario/password_hacker.rb
@@ -2,21 +2,23 @@ nombre_archivo_diccionario = ARGV[0]
 palabra_a_hackear = ARGV[1]
 
 =begin 
-1- buscar si la palabra ingresada está en el diccionario "imprimir"
 2- probar cada palabra del diccionario mas 1 12 123 "imprimir"
 3- si no está avisar y probar forma ya resuelta 
 imprimir iteraciones
 =end
 def busqueda_dic(nombre_archivo_diccionario, palabra_a_hackear)  
-    reglones = File.readlines("diccionario.txt")
-    counter = 0
-    reglones.each do |i|
-        if reglones == palabra_a_hackear
+    reglones = File.readlines(nombre_archivo_diccionario)
+    n = reglones.count
+    counter = 1
+    n.times do |i|
+        if reglones[i].chomp == palabra_a_hackear
             puts "Encontrada al intento #{counter}"
+            counter += 1
+            exit
         end
-        counter += 1
+        print counter += 1
     end
-    puts counter
+    #puts counter
 end
 def busqueda_dic_apliada
 end
