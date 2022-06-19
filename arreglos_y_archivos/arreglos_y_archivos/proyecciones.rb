57d1b95e2572d6d114299213e06c8ce19e159b4b
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-18 21:10:57 -0400

    Desarrollando proyeccciones

--- arreglos_y_archivos/arreglos_y_archivos/proyecciones.rb
+++ arreglos_y_archivos/arreglos_y_archivos/proyecciones.rb
@@ -4,12 +4,21 @@ data.each do |j|
     ventas.push j.to_f
 end 
 print ventas
+
 #Primera mitad del primer semestre aumenta 10% más.
 #la segunda mitad aumenta 20% más.
 
 ventas_aumentadas = []
 ventas.each do |j|
-    ventas_aumentadas.push((j*1.1).round(2))
+    ventas_aumentadas.push((j).round(2))   
 end
-print ventas_aumentadas#.round(2)
+#print ventas_aumentadas
 
+(0..2).each do |i|
+    ventas_aumentadas[i] = (ventas_aumentadas[i]*1.1).round(2)
+end
+print ventas_aumentadas
+(9..11).each do |i|
+    ventas_aumentadas[i] = (ventas_aumentadas[i]*1.2).round(2)
+end
+print ventas_aumentadas
