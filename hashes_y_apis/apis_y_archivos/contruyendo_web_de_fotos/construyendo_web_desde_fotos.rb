f151671a61f69bdc6e8cb34769d0f538c25914b4
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-22 10:00:31 -0400

    Actualizando construyendo web

--- hashes_y_apis/apis_y_archivos/contruyendo_web_de_fotos/construyendo_web_desde_fotos.rb
+++ hashes_y_apis/apis_y_archivos/contruyendo_web_de_fotos/construyendo_web_desde_fotos.rb
@@ -14,9 +14,14 @@ def request(url_requested)
 end
 def build_web_page(hash)
     photos = hash.map{|x| x['url']}
-    html = "
-<html>
+    html = 
+    "<!DOCTYPE html>
+<html lang='en'>
 <head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
 </head>
 <body>
 <ul>
