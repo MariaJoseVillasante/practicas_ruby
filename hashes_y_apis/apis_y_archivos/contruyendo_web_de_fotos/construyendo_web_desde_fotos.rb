9b63e4f6aeb1219f52c6fd97e93fa41ccf8964fb
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-22 09:29:14 -0400

    Agregando construyendo web desde fotos

--- hashes_y_apis/apis_y_archivos/contruyendo_web_de_fotos/construyendo_web_desde_fotos.rb
+++ hashes_y_apis/apis_y_archivos/contruyendo_web_de_fotos/construyendo_web_desde_fotos.rb
@@ -5,15 +5,30 @@ def request(url_requested)
     url = URI(url_requested)
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
+    https.verify_mode = OpenSSL::SSL::VERIFY_PEER
     request = Net::HTTP::Get.new(url)
+    request["cache-control"] = 'no-cache'
+    request["postman-token"] = '5f4b1b36-5bcd-4c49-f578-75a752af8fd5'
     response = https.request(request)
-    JSON.parse(response.read_body)
+    return JSON.parse(response.body)
 end
-data = request("https://jsonplaceholder.typicode.com/photos")[0..10]
-photos = data.map{|x| x['url']}
-html = ""
-photos.each do |photo|
-    html += "<img scr=\"#{photo}\">\n"
-end
-File.write('output.html',html)
-#print html
+def build_web_page(hash)
+    photos = hash.map{|x| x['url']}
+    html = "
+<html>
+<head>
+</head>
+<body>
+<ul>
+"
+    photos.each do |photo|
+        html += "    <li><img scr=\"#{photo}\"></li>\n"
+    end
+    html += "</ul>
+</body>
+</html>
+    "
+    File.write('output.html',html)
+end 
+data = request("https://jsonplaceholder.typicode.com/photos")
+build_web_page(data[0..12])
